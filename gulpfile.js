117943a898479fe8b518efbd37f09e0d9920fc4a
Kieran Devlin <kjrdevlin@gmail.com>
2019-10-25 16:30:24 -0700

    integrate scss into json, gulp, and root directories.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ const gulp = require("gulp"),
     terser = require("gulp-terser"),
     rename = require("gulp-rename"),
     browserSync = require("browser-sync"),
-    eslint = require("gulp-eslint");
+    eslint = require("gulp-eslint"),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    prettyError = require('gulp-prettyerror');
 
 gulp.task("lint", function () {
     return gulp
@@ -11,6 +15,18 @@ gulp.task("lint", function () {
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 
+});
+
+gulp.task('sass', function () {
+    return gulp
+        .src('./sass/style.scss')
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./build/css'))
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task("scripts",
@@ -22,6 +38,7 @@ gulp.task("scripts",
                 .pipe(rename({ extname: ".min.js" }))
                 .pipe(gulp.dest("./build/js"));
         })
+
 );
 
 gulp.task('browser-sync', function () {
@@ -31,12 +48,13 @@ gulp.task('browser-sync', function () {
         }
     });
 
-    gulp.watch(["*.html", "css/*.css", "build/js/*.js"])
+    gulp.watch(["*.html", "build/css/*.css", "build/js/*.js"])
         .on("change", browserSync.reload);
 });
 
 gulp.task("watch", function () {
     gulp.watch("js/*.js", gulp.series("scripts"));
+    gulp.watch("sass/*.scss", gulp.series("sass"));
 });
-
+//gulp.watch(['js/*.js', 'sass/*.scss'], gulp.parallel('scripts', 'sass'))
 gulp.task("default", gulp.parallel("browser-sync", "watch"));
