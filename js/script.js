41ddc56f99068de3c8c93842980ab54b394848df
Kieran Devlin <kjrdevlin@gmail.com>
2019-10-28 16:45:50 -0700

    update api data to use css grid in lieu of flexbox - add layout partial to scss, update js to execute filter method instead of if/else to check if articles have images, fix path to broken embedded font links

--- js/script.js
+++ js/script.js
@@ -11,21 +11,54 @@ $('#selection').on('change', function () {
         dataType: 'json'
     })
         .done(function (data) {
-            console.log(data.results);
-            $.each(data.results.splice(0, 12), function (key, value) {
+            // console.log(data.results);
 
 
-                if (typeof value.multimedia[4] === 'undefined' || value.multimedia[4] === null) {
-                    return;
-                }
-                else {
-                    $('.story-container').append(`<div style='background-image:url(${value.multimedia[4].url});'> <a href='${value.short_url}'> ${value.abstract} </a> </div>`);
-                    $('.story-container').children('div').addClass('nytimes-story');
-                    $('.story-container').children('div').children('a').addClass('text-box');
-                    //THIS IS THE WRONG WAY TO DO IT --> $('.nytimes-story').css('background-image', 'url("' + value.multimedia[4].url + '")');
-                }
+            const filteredResults = data.results.filter(function (articleObject) {
+                return articleObject.multimedia[4] !== 'undefined';
+            }).slice(0, 12);
 
+            console.log("filtered results", filteredResults);
+
+
+            $.each(filteredResults, function (index, value) {
+
+
+                // console.log(data.results[0].multimedia[4].url);
+
+                $('.story-container').append(`<div style='background-image:url(${value.multimedia[4].url});'> <a href='${value.short_url}'> ${value.abstract} </a> </div>`);
+                $('.story-container').children('div').addClass('nytimes-story');
+                $('.story-container').children('div').children('a').addClass('text-box');
             });
+            // data.results
+            // .filter(
+            //     this.multimedia[4].url === false
+            //     function () {
+            //     $(this.multimedia[4].url).filter(function () {
+            //         this.multimedia[4].url !== 'undefined'
+            //     })
+
+            // }
+            // )
+            // .splice(0, 12), function (key, value) {
+
+
+
+            // if (typeof value.multimedia[4] === 'undefined' || value.multimedia[4] === null) {
+            //     return;
+            // }
+            // // else {
+            // console.log(data.results[0].multimedia[4].url);
+
+            // $('.story-container').append(`<div style='background-image:url(${value.multimedia[4].url});'> <a href='${value.short_url}'> ${value.abstract} </a> </div>`);
+            // $('.story-container').children('div').addClass('nytimes-story');
+            // $('.story-container').children('div').children('a').addClass('text-box');
+            //THIS IS THE WRONG WAY TO DO IT --> $('.nytimes-story').css('background-image', 'url("' + value.multimedia[4].url + '")');
+            // }
+
+
+            // }
+            // );
 
 
         });
